d6a84da399d10d2386a5036a050fbb8d24b31caa
yyoshiaki <y.yasumizu@gmail.com>
2020-08-02 18:26:42 +0900

    wrapper run.sh minor change

--- wrapper/run.sh
+++ wrapper/run.sh
@@ -1,5 +1,12 @@
+#!/bin/bash
+set -xe
+
+DIR_VIRTUS=$HOME/Programs/VIRTUS
+DIR_INDEX_ROOT=$HOME/media32TB/bioinformatics/reference/VIRTUS_200803
+
 python3 VIRTUS_wrapper.py input.csv \
-    --VIRTUSDir ~/bioinformatics/VIRTUS \
-    --genomeDir_human ~/bioinformatics/VIRTUS/index/STAR_index_human \
-    --genomeDir_virus ~/bioinformatics/VIRTUS/index/STAR_index_virus \
-    --salmon_index_human ~/bioinformatics/VIRTUS/index/salmon_index_human
+    --VIRTUSDir $DIR_VIRTUS \
+    --genomeDir_human $DIR_INDEX_ROOT/STAR_index_human \
+    --genomeDir_virus $DIR_INDEX_ROOT/STAR_index_virus \
+    --salmon_index_human $DIR_INDEX_ROOT/salmon_index_human \
+    --nthreads=4
