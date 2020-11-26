01534809718c47101c11b81bb21c9c923cd9fe1f
yyoshiaki <y.yasumizu@gmail.com>
2020-11-26 17:19:50 +0900

    update mk_summary_virus_count.py

--- tool/mk_summary_virus_count/mk_summary_virus_count.py
+++ tool/mk_summary_virus_count/mk_summary_virus_count.py
@@ -20,12 +20,15 @@ df_STARLog = pd.read_csv(input_STARLog, sep='\t', header=None, index_col=0)
 num_reads = int(df_STARLog.loc['                   Uniquely mapped reads number |', 1]) + \
             int(df_STARLog.loc['        Number of reads mapped to multiple loci |', 1])
 
-df_virus_count = pd.read_csv(input_virus_count, delim_whitespace=True)
-df_virus_count.columns = ['num_hit', 'virus']
-if input_layout == 'PE':
-    df_virus_count['num_hit'] = df_virus_count['num_hit'] / 2
-df_virus_count['rate_hit'] = df_virus_count['num_hit'] / num_reads
-df_virus_count = df_virus_count[['virus', 'num_hit', 'rate_hit']]
-df_virus_count = df_virus_count.sort_values(by='rate_hit', ascending=False)
+try:
+    df_virus_count = pd.read_csv(input_virus_count, delim_whitespace=True)
+    df_virus_count.columns = ['num_hit', 'virus']
+    if input_layout == 'PE':
+        df_virus_count['num_hit'] = df_virus_count['num_hit'] / 2
+    df_virus_count['rate_hit'] = df_virus_count['num_hit'] / num_reads
+    df_virus_count = df_virus_count[['virus', 'num_hit', 'rate_hit']]
+    df_virus_count = df_virus_count.sort_values(by='rate_hit', ascending=False)
+except:
+    df_virus_count = pd.DataFrame(columns=['virus', 'num_hit', 'rate_hit'])
 
 df_virus_count.to_csv('virus.counts.final.tsv', index=None, sep='\t')
